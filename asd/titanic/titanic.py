6542060e99eb22843b45c85cae3e1c29119bce7b
Dudás Alexandra <dudasa20@ADY-NAGYATAD.SULINET.HU>
2022-03-23 08:59:01 +0100

    ...

--- asd/titanic/titanic.py
+++ asd/titanic/titanic.py
@@ -6,3 +6,15 @@ f.close()
 print("2. feladat: " + str (len(adatok)) + "db")
 
 print ([e.split(";")[1] for e in adatok])
+ossz=0
+for e in adatok:
+    temp=e.split(";")
+    tulelo=int(temp[1])
+    eltunt=int(temp[2])
+    pssz+=tulelo
+    ossz+=eltunt
+
+print("3.feladat: " +str(ossz) + "fõ")
+
+kulcsSzo=input("4.feladat: Kulcsszó: ")
+van=false
