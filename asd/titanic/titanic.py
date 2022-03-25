87275cb6e1585fc282e3ce99e081ca392e58d63f
Dudás Alexandra <dudasa20@ADY-NAGYATAD.SULINET.HU>
2022-03-25 09:37:11 +0100

    asd123

--- asd/titanic/titanic.py
+++ asd/titanic/titanic.py
@@ -11,10 +11,45 @@ for e in adatok:
     temp=e.split(";")
     tulelo=int(temp[1])
     eltunt=int(temp[2])
-    pssz+=tulelo
+    ossz+=tulelo
     ossz+=eltunt
 
 print("3.feladat: " +str(ossz) + "fõ")
 
 kulcsSzo=input("4.feladat: Kulcsszó: ")
 van=false
+
+for e in adatok:
+    if e.find(kulcsSzo)>=0:
+        van=True
+        break
+
+if van:
+    print("Van találat")
+else:
+    print("Nincs találat")
+
+
+adatok2=[]
+for e in adatok:
+    temp=e.split(";")
+    temp[1]=int(temp[1])
+    temp[2]=int(temp[2])
+    adatok2.append(temp)
+print(adatok2)
+
+talalat=[e for e in adatok if e[0].find(kulcsSzo)>-1]
+print(talalat)
+if len(talalat)>0:
+    print("Van találat")
+else:
+    print("Nincs találat")
+
+##
+print("5.feladat")
+for e in talalat:
+    print(e[0]+ " " + str(e[1]+e[2])+" fõ")
+
+
+print([ e[0]+ " " + str(e[1]+e[2])+" fõ" for e in talalat2 ])
+
