de63d04911c528a23de65169c1356452c9cf3356
Dudás Alexandra <dudasa20@ADY-NAGYATAD.SULINET.HU>
2022-04-04 12:31:38 +0200

    7.

--- hiányzások/hianyzasok.py
+++ hiányzások/hianyzasok.py
@@ -33,3 +33,29 @@ for e in naplo:
 
 igazolt=[e[3].count("X") for e in naplo]
 print("3. feladat\nAz igazolt hiányzások száma" + str(igazolt) + " ,az igazolatlanoké" + str(igazolatlan) + " óra.")
+#3. feladat vége
+
+#4. feladat
+def hetnapja(honap,nap):
+    napnev =["vasarnap", "hetfo", "kedd", "szerda", "csutortok", "pentek", "szombat"]
+    napszam = (0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 335)
+    napsorszam = (napszam[honap-1] + nap) % 7
+    return napnev[napsorszam]
+#4. feladat vége
+
+#5. feladat
+honap=int(input("A hónap sorszáma: "))
+nap=int(input=("A nap sorszáma: "))
+print("Azon a napon" + hetnapja(honap,nap) + " volt.")
+#5.feladat vége
+
+#6.feladat
+napnev=input("a nap neve= ")
+ora=int(input("Az óra sorszáma: "))
+
+db=0
+for e in naplo:
+    if hetnapja(e[0], e[1])==
+
+
+#7. 
