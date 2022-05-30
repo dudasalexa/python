b379544e5be218c5a1cf631554d73ea90f48086d
dudasalexa <80906214+dudasalexa@users.noreply.github.com>
2022-05-30 08:45:28 +0200

    io
    io

---
+++ sms/smsmodul.py
@@ -0,0 +1,9 @@
+class adatok:
+    def __init__(self,sor1,sor2):
+        vag=sor1.split(" ")
+        self.ora=int(vag[0])
+        self.perc=int(vag[1])
+        self.telefonszam=vag[2]
+
+        self.uzenet=sor2
+        
